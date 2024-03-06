490759432b2eeaeb50ef42732b563cb0efab378d
Xinyuuu <xinyu.zhang@tum.de>
2024-03-06 14:35:16 +0100

    Build game screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query'
+import { Screenshot } from '../entities/Screenshot'
+import ApiClient, { FetchResponse } from '../services/api-client'
+
+const useScreenshots = (gameId: number) => {
+  const apiClient = new ApiClient<Screenshot>(`/games/${gameId}/screenshots`)
+
+  const fetchScreenshots = () => apiClient.getAll()
+
+  return useQuery<FetchResponse<Screenshot>>({
+    queryKey: ['screenshots', gameId],
+    queryFn: fetchScreenshots,
+  })
+}
+
+export default useScreenshots
